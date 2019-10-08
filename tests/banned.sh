38a6ad1845613773517cc21fe6942fd88ccc71a8
vswamina <vswamina@ucsc.edu>
2019-10-08 16:19:49 -0700

    moved new script into tests directory

--- tests/banned.sh
+++ tests/banned.sh
@@ -3,4 +3,4 @@ badfile=$1
 badwords=$(paste -s -d '|' $badfile)
 
 echo searching for banned keywords!
-grep --only-matching -P '^.*?(?=(\-\-|$))' src/* | grep --color -P '\b('$badwords')\b'
+grep --only-matching -n -H -P '^.*?(?=(\-\-|$|import))' src/* | grep --color -P '\b('$badwords')\b'

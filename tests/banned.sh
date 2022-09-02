7a750ac3590d4509fcbcf73db8d48652dc594f6e
Owen Arden <owenarden@gmail.com>
2022-09-02 16:27:47 -0700

    use portable grep args

--- tests/banned.sh
+++ tests/banned.sh
@@ -3,4 +3,5 @@ badfile=$1
 badwords=$(paste -s -d '|' $badfile)
 
 echo searching for banned keywords!
-grep --only-matching -n -H -P '^.*?(?=(\-\-|$|import))' src/* | grep --color -P '\b('$badwords')\b'
+grep -H -n '.*' src/* | sed 's:import.*$::g' | sed 's:--.*$::g' | grep --color -E '[[:<:]]('$badwords')[[:>:]]'
+

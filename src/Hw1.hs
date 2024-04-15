0887412e228e77553742199e630331d59a5fcb82
Jonathan Castello <twisolar@gmail.com>
2024-04-14 22:00:18 -0700

    Replace digital persistence with credit card validation

--- src/Hw1.hs
+++ src/Hw1.hs
@@ -17,19 +17,36 @@ module Hw1 where
 import Prelude  hiding (replicate, sum, reverse)
 
 
--- | Sum the elements of a list
+-- | Reverse the order of elements in a list; in other words,
+--   `listReverse [x1, x2, ..., xn]` returns `[xn, ..., x2, x1]`.
 --
--- >>> sumList [1, 2, 3, 4]
--- 10
+-- >>> listReverse [1,2,3,4]
+-- [4,3,2,1]
 --
--- >>> sumList [1, -2, 3, 5]
--- 7
+-- >>> listReverse ["i", "want", "to", "ride", "my", "bicycle"]
+-- ["bicycle", "my", "ride", "to", "want", "i"]
 --
--- >>> sumList [1, 3, 5, 7, 9, 11]
--- 36
+-- >>> listReverse []
+-- []
 
-sumList :: [Int] -> Int
-sumList xs = error "TBD:sumList"
+listReverse :: [a] -> [a]
+listReverse xs = error "TBD:listReverse"
+
+
+-- | Determine whether a string is a palindrome (i.e. spelled the same
+--   both backwards and forwards).
+--
+-- >>> palindrome "malayalam"
+-- True
+--
+-- >>> palindrome "palindrome"
+-- False
+--
+-- >>> palindrome ""
+-- True
+
+palindrome :: String -> Bool
+palindrome w = error "TBD:palindrome"
 
 
 -- | `digitsOfInt n` should return `[]` if `n` is not positive,
@@ -41,83 +58,65 @@ sumList xs = error "TBD:sumList"
 --
 -- >>> digitsOfInt 352663
 -- [3, 5, 2, 6, 6, 3]
+--
+-- >>> digitsOfInt -42
+-- []
 
-digitsOfInt :: Int -> [Int]
+digitsOfInt :: Integer -> [Integer]
 digitsOfInt n = error "TBD:digitsOfInt"
 
 
--- | `digits n` returns the list of digits of `n`
+-- | `digitsOfInts xs` should return a list containing all of the digits
+--   for every number in `xs`, in the same order they appeared in `xs`.
 --
--- >>> digits 31243
--- [3,1,2,4,3]
+-- >>> digitsOfInts [3124, 52, -42, 8]
+-- [3, 1, 2, 4, 5, 2, 8]
 --
--- digits (-23422)
--- [2, 3, 4, 2, 2]
-
-digits :: Int -> [Int]
-digits n = digitsOfInt (abs n)
-
-
--- | From http://mathworld.wolfram.com/AdditivePersistence.html
---   Consider the process of taking a number, adding its digits,
---   then adding the digits of the number derived from it, etc.,
---   until the remaining number has only one digit.
---   The number of additions required to obtain a single digit
---   from a number n is called the additive persistence of n,
---   and the digit obtained is called the digital root of n.
---   For example, the sequence obtained from the starting number
---   9876 is 9876 -> 30 -> 3, so 9876 has
---   an additive persistence of 2 and
---   a digital root of 3.
---
--- NOTE: assume additivePersistence & digitalRoot are only called with positive numbers
-
--- >>> additivePersistence 9876
--- 2
-
--- >>> additivePersistence 99999
--- 2
-
-additivePersistence :: Int -> Int
-additivePersistence n = error "TBD"
-
--- | digitalRoot n is the digit obtained at the end of the sequence
---   computing the additivePersistence
-
--- >>> digitalRoot 9876
--- 3
-
--- >>> digitalRoot 99999
--- 9
-
-digitalRoot :: Int -> Int
-digitalRoot n = error "TBD"
-
-
--- | listReverse [x1,x2,...,xn] returns [xn,...,x2,x1]
---
--- >>> listReverse []
+-- >>> digitsOfInts []
 -- []
---
--- >>> listReverse [1,2,3,4]
--- [4,3,2,1]
---
--- >>> listReverse ["i", "want", "to", "ride", "my", "bicycle"]
--- ["bicycle", "my", "ride", "to", "want", "i"]
 
-listReverse :: [a] -> [a]
-listReverse xs = error "TBD"
+digitsOfInts :: [Integer] -> [Integer]
+digitsOfInts xs = error "TBD:digitsOfInts"
 
--- | In Haskell, a `String` is a simply a list of `Char`, that is:
+
+-- | Doubles every other integer in a list,
+--   starting with the second element.
 --
--- >>> ['h', 'a', 's', 'k', 'e', 'l', 'l']
--- "haskell"
+-- >>> doubleEveryOther [8,7,6,5]
+-- [8,14,6,10]
 --
--- >>> palindrome "malayalam"
+-- >>> doubleEveryOther [1,2,3]
+-- [1,4,3]
+--
+-- >>> doubleEveryOther []
+-- []
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = error "TBD:doubleEveryOther"
+
+
+-- | Sum the elements of a list
+--
+-- >>> sumList [1, 2, 3, 4]
+-- 10
+--
+-- >>> sumList [1, -2, 3, 5]
+-- 7
+--
+-- >>> sumList [1, 3, 5, 7, 9, 11]
+-- 36
+
+sumList :: [Integer] -> Integer
+sumList xs = error "TBD:sumList"
+
+
+-- | Validate a credit card number
+--
+-- >>> validateCardNumber 4012888888881881
 -- True
 --
--- >>> palindrome "myxomatosis"
+-- >>> validateCardNumber 4012888888881882
 -- False
 
-palindrome :: String -> Bool
-palindrome w = error "TBD"
+validateCardNumber :: Integer -> Bool
+validateCardNumber = error "TBD:validateCardNumber"

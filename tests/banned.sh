d3d7fc510327df296b50c37d6521023f2099f02f
Lucas Ellenberger <lmellenb@ucsc.edu>
2025-03-11 15:36:06 -0700

    Updated check for taboo words.

--- tests/banned.sh
+++ tests/banned.sh
@@ -3,5 +3,9 @@ badfile=$1
 badwords=$(paste -s -d '|' $badfile)
 
 echo searching for banned keywords!
-grep -H -n '.*' src/* | sed 's:import.*$::g' | sed 's:--.*$::g' | grep --color -E '[[:<:]]('$badwords')[[:>:]]'
-
+if grep -H -n '.*' src/* | sed 's:import.*$::g' | sed 's:--.*$::g' | grep --color -E '\b('$badwords')\b'; then
+    echo "ERROR: Banned identifiers found (see tests/taboo for a list)."
+    exit 1
+else
+    exit 0
+fi

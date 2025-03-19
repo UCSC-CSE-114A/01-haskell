fae966c696c15083a0f7c033c9bfa42838a11e75
Lucas Ellenberger <lmellenb@ucsc.edu>
2025-03-19 15:40:40 -0700

    Improved script to check for taboo words.

--- tests/banned.sh
+++ tests/banned.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-badfile=$1
-badwords=$(paste -s -d '|' $badfile)
 
-echo searching for banned keywords!
-if grep -H -n '.*' src/* | sed 's:import.*$::g' | sed 's:--.*$::g' | grep --color -E '\b('$badwords')\b'; then
-    echo "ERROR: Banned identifiers found (see tests/taboo for a list)."
-    exit 1
-else
-    exit 0
-fi
+function main() {
+    if [[ $# -ne 1 ]]; then
+        echo "USAGE: $0 <taboo word file>"
+        return 1
+    fi
+
+    trap exit SIGINT
+
+    local taboo_file=$1
+    shift 1
+
+    local taboo_words=$(paste -s -d '|' $taboo_file)
+
+    echo 'Searching for banned keywords!'
+    output=$(grep -H -n '.*' src/* | sed 's:import.*$::g' | sed 's:--.*$::g' | grep --color -E "\b($taboo_words)\b")
+
+    local exit_status=$?
+    if [[ ${exit_status} -eq 0 ]]; then
+        echo "ERROR: Banned identifiers found (see '${taboo_file}' for a list)."
+        echo 'Found the following issues:'
+        echo "${output}"
+        return 1
+    else
+        echo 'No issues found!'
+        return 0
+    fi
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
